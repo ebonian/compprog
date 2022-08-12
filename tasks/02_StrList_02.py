1722d756a2c7de245629a59f96ca92b6847723fc
Ebonian <tanadon.santisan@gmail.com>
2022-08-12 14:27:39 +0700

    add task 02 solution 02

---
+++ tasks/02_StrList_02.py
@@ -0,0 +1,8 @@
+# assign all the words to words list
+words = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+# take the input of index
+index = int(input())
+
+# print the result
+print("%d --> %s" % (index, words[index]))
