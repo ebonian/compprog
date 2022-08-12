301f0dee61d6c5e11840b451c3fb7469f90a85cd
Ebonian <tanadon.santisan@gmail.com>
2022-08-12 12:35:36 +0700

    fix task 01 expr 06 time different calculation

--- tasks/01_Expr_06.py
+++ tasks/01_Expr_06.py
@@ -13,20 +13,12 @@ t1 = h1*60*60 + m1*60 + s1
 t2 = h2*60*60 + m2*60 + s2
 
 # calculate time different
-dt = t2-t1
+dt = (t2-t1) if t2 > t1 else (24*60*60 + t2-t1)
 dh = dt // (60*60)
 dt -= dh * 60*60
 dm = dt//60
 dt -= dm*60
 ds = dt
 
-# check if second time is more than first time, if not minus hour different with 24 hrs
-if t2 > t1:
-  print(str(dh)+":"+str(dm)+":"+str(ds))
-else:
-  dh = 24 - abs(dh)
-  print(str(dh)+":"+str(dm)+":"+str(ds))
-
-# print(dh)
-# print(dm)
-# print(ds)
+# print the result
+print(str(dh)+":"+str(dm)+":"+str(ds))
