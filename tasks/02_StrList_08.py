3cd6e44807838f1079382a817b1f571c1e7ca1ac
Ebonian <tanadon.santisan@gmail.com>
2022-08-15 23:00:05 +0700

    Update 02_StrList_08.py

--- tasks/02_StrList_08.py
+++ tasks/02_StrList_08.py
@@ -4,12 +4,11 @@ def findFraction(decimal: float, point: int, repeat: int):
   X = decimal
   nX = decimal * math.pow(10, repeat)
 
-  X = math.floor(X * 10 ** point) / 10 ** point
-  nX = math.floor(nX * 10 ** point) / 10 ** point
+  X = math.floor(X * math.pow(10, point)) / math.pow(10, point)
+  nX = math.floor(nX * math.pow(10, point)) / math.pow(10, point)
   n = nX - X
   d = math.pow(10, repeat) - 1
-
-  numerator = n
+  numerator = round(n, point)
   denominator = d
 
   numerator *= math.pow(10, len(str(n).split(".")[1]))
@@ -17,20 +16,24 @@ def findFraction(decimal: float, point: int, repeat: int):
 
   return [numerator, denominator]
 
-def findGCD(n, d):
+def findGCD(n, d) -> int:
   n = int(n)
   d = int(d)
   res = math.gcd(n, d)
   return res
 
-decimal = input()
-decimalList = decimal.split(",")
-decimal = float(decimalList[0] + "." + decimalList[1] + decimalList[2] + decimalList[2])
+def main():
+  decimal = input()
+  decimalList = decimal.split(",")
+  decimal = float(decimalList[0] + "." + decimalList[1] + decimalList[2] + decimalList[2])
+  decimal = abs(decimal)
 
-decimal = findFraction(decimal, len(decimalList[1]), len(decimalList[2]))
-devisor = findGCD(decimal[0], decimal[1])
+  decimal = findFraction(decimal, len(decimalList[1]), len(decimalList[2]))
+  numerator = decimal[0]
+  denominator = decimal[1]
 
-numerator = decimal[0] / devisor
-denominator = decimal[1] / devisor
+  divisor = findGCD(numerator, denominator)
 
-print("%d / %d" % (int(numerator), int(denominator)))
+  print("%d / %d" % (int(numerator / divisor), int(denominator / divisor)))
+
+main()
