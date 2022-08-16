9aa5dba25f385047a8ab882e78b393d7159ab0e8
Ebonian <tanadon.santisan@gmail.com>
2022-08-16 20:11:38 +0700

    commented

--- tasks/02_StrList_08.py
+++ tasks/02_StrList_08.py
@@ -1,14 +1,27 @@
 import math
 
+# take the input of a b and c and split it for each value
 [a, b, c] = input().split(",")
 
-ib = b
+# initial value of b
+initial_b = b
+
+# check if b, if not return 0
 b = b if b else "0"
 
-n = int(b+c) - int(b)
-d = int(''.join(map(str,([9] * len(c)) + [0] * len(ib))))
-n = d * int(a) + n
+# calculate the numerator by joining string of decimal (b) and repeating decimal (c), convert to integer and minus by decimal that is not repeating (b)
+numerator = int(b + c) - int(b)
 
-divisor = math.gcd(n, d)
+# calculate the denominator by convert each repeating decimal (c) to 9 with length of repeating decimal times (len(c)), then join with 0 repeat by length of b times (len(initial_b))
+denominator = int(''.join(map(str,([9] * len(c)) + ([0] * len(initial_b)))))
 
-print("%d / %d" % (n//divisor, d//divisor))
+# calculate the exceed fraction to the denominator
+numerator = denominator * int(a) + numerator
+
+# find the divisor using math.gcd
+divisor = math.gcd(numerator, denominator)
+
+# divide numerator and denominator by gcd and print out the solution
+print("%d / %d" % (numerator//divisor, denominator//divisor))
+
+# formula from -> https://www.opendurian.com/news/mathcirdec/
