9e3645750bf95c9ccfe7f06821fcfb4cce785c22
Ebonian <tanadon.santisan@gmail.com>
2022-08-16 19:07:59 +0700

    all solution

--- tasks/P1_07_Biorhythm.py
+++ tasks/P1_07_Biorhythm.py
@@ -1,20 +1,36 @@
 import math
-from datetime import datetime
+
+mDay = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 bd, bm, by, d, m, y = [int(e) for e in input().split()]
 
 by -= 543
 y -= 543
 
+# check if by is leap year
+if (by%4==0 and by%100!=0 or by%400==0):
+  mDay[1] = 29
 
-bDate = datetime(by, bm, bd)
-date = datetime(y, m, d)
+red = mDay[bm - 1] - bd + 1 + sum(mDay[bm:])
 
-dateDiff = date - bDate
-print(dateDiff.days)
+# reset
+mDay[1] = 28
 
-p = math.sin((2 * math.pi * dateDiff.days) / (23))
-e = math.sin((2 * math.pi * dateDiff.days) / (28))
-i = math.sin((2 * math.pi * dateDiff.days) / (33))
+black = 365 * (y - by - 1)
 
-print(dateDiff.days, "{:.2f}".format(p), "{:.2f}".format(e), "{:.2f}".format(i))
+# check if y is leap year
+if (y%4==0 and y%100!=0 or y%400==0):
+  mDay[1] = 29
+
+blue = sum(mDay[:m - 1]) + d - 1
+
+# reset
+mDay[1] = 28
+
+dateDiff = red + black + blue
+
+p = math.sin((2 * math.pi * dateDiff) / (23))
+e = math.sin((2 * math.pi * dateDiff) / (28))
+i = math.sin((2 * math.pi * dateDiff) / (33))
+
+print(dateDiff, "{:.2f}".format(p), "{:.2f}".format(e), "{:.2f}".format(i))
