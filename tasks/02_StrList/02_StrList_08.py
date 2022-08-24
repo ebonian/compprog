44c0a6c9d3b4f0d97f43dd2e573a588b3bdcf0c9
Ebonian <tanadon.santisan@gmail.com>
2022-08-24 14:20:58 +0700

    no if in solution 8 task 2

--- tasks/02_StrList/02_StrList_08.py
+++ tasks/02_StrList/02_StrList_08.py
@@ -3,17 +3,11 @@ import math
 # take the input of a b and c and split it for each value
 [a, b, c] = input().split(",")
 
-# initial value of b
-initial_b = b
-
-# check if b, if not return 0
-b = b if b else "0"
-
 # calculate the numerator by joining string of decimal (b) and repeating decimal (c), convert to integer and minus by decimal that is not repeating (b)
-numerator = int(b + c) - int(b)
+numerator = int("0" + b + c) - int("0" + b)
 
 # calculate the denominator by convert each repeating decimal (c) to 9 with length of repeating decimal times (len(c)), then join with 0 repeat by length of b times (len(initial_b))
-denominator = int(''.join(map(str,([9] * len(c)) + ([0] * len(initial_b)))))
+denominator = int(''.join(map(str,([9] * len(c)) + ([0] * len(b)))))
 
 # calculate the exceed fraction to the denominator
 numerator = denominator * int(a) + numerator
