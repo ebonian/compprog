4bffbf9b48e4b2c62cb9f07752845c42fba1cabe
Ebonian <tanadon.santisan@gmail.com>
2022-08-24 13:56:49 +0700

    04 task solution

--- tasks/04/04_Loop_002.py
+++ tasks/04/04_Loop_002.py
@@ -1,20 +1,18 @@
 d = input().split(" ")
 
 d = [int(x) for x in d]
-n = len(d)
-p = d[n-1]
+p = d[-1]
 i = -1
 j = 0
+n = len(d)
 
-while 1:
-  if (j < n - 1):
-    if (d[j] <= p):
-      i += 1
-      d[i], d[j] = d[j], d[i]
-    j += 1
-  else:
+while True:
+  if (d[j] <= p):
+    i += 1
+    d[i], d[j] = d[j], d[i]
+  j+=1
+
+  if (j>=n):
     break
 
-d[i], d[-1] = d[-1], d[i]
-
-print(d, i)
+print(d)
