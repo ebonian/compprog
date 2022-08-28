aeb7e03e2e8f5a7917f1f5691ce4d1329966e089
Ebonian <tanadon.santisan@gmail.com>
2022-08-28 21:13:25 +0700

    commenting

--- tasks/04_Loop/04_Loop_06.py
+++ tasks/04_Loop/04_Loop_06.py
@@ -4,24 +4,24 @@ height = int(input())
 space = height
 
 for i in range(height):
-    # start the index by 1 not 0
-    i += 1
+	# start the index by 1 not 0
+	i += 1
 
-    # space before triangle border
-    for j in range(space):
-        print(" ", end="")
+	# space before triangle border
+	for j in range(space):
+		print(" ", end="")
 
-    # star and space inside triangle
-    for k in range(1, i*2):
-        # triangle border
-        if k==1 or k == 2 * i - 1 or i == height :
-            print("*", end="")
-        # space inside
-        else:
-            print(" ", end="")
+	# star and space inside triangle
+	for k in range(1, i*2):
+		# triangle border
+		if (k==1 or k == 2 * i - 1 or i == height):
+			print("*", end="")
+		# space inside
+		else:
+			print(" ", end="")
 
-    # decrease space before trangle by 1
-    space -= 1
-    
-    # print next line
-    print()
+	# decrease space before trangle by 1
+	space -= 1
+	
+	# print next line
+	print("")
--- tasks/05_List/05_List_23.py
+++ tasks/05_List/05_List_23.py
@@ -1,25 +1,43 @@
+# import math module
 import math
 
+# take the interger input of n for the numbers of points
 n_points = int(input())
 
+# assign list of point and list of distance for append from input
 points = []
 distances = []
-obj = {}
 
+# assign distance_dict dictionary for store the distance for each point
+distance_dict = {}
+
+# loop through n_points to take the input of points n_points times
 for n in range(n_points):
+  # take the input and split it into list then append its list to points list
   points.append(input().split(" "))
 
-# calculate distance
+# calculate distance start by loop through list of points by its index
 for i in range(len(points)):
+  # extract point position by x and y from list of points according to the index
   x, y = points[i]
 
-  distance = math.sqrt(math.pow(float(x) - 0, 2) + math.pow(float(y) - 0, 2))
+  # convert point x and y to float from string
+  x, y = float(x), float(y)
 
-  obj[i + 1] = distance
+  # calculate the distance
+  distance = math.sqrt(math.pow(x - 0, 2) + math.pow(y - 0, 2))
 
+  # assign the distance into dictionary of distance_dict by the key of its index + 1
+  distance_dict[i + 1] = distance
+
+  # append distance to list of distances
   distances.append(distance)
 
+# slice the third distance from list of distances by sort it and select the value by the index of 2
 third_distance = sorted(distances)[2]
-distance_index = list(obj.keys())[list(obj.values()).index(third_distance)]
 
+# assign distance_index by take the key from the thrid distance value from distance_dict
+distance_index = list(distance_dict.keys())[list(distance_dict.values()).index(third_distance)]
+
+# print out the result of third-closest point index and position
 print("#%d: (%s)" % (distance_index, ', '.join(points[distance_index - 1])))
